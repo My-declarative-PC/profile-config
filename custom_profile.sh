116c122abc836db105d4d77783245c2996e896ac
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2025-01-14 10:08:37 +0300

    Исправил глобальные переменные
    поскольку Helix ставится из brew в указании полного пути и рантайма не
    тсмысла

--- custom_profile.sh
+++ custom_profile.sh
@@ -16,7 +16,6 @@ export FLAVOR="mocha"
 export ACCENT="mauve"
 export GTK_THEME=catppuccin-$ACCENT-$FLAVOR-standard+default
 
-export EDITOR=/usr/bin/hx
-export HELIX_RUNTIME=/var/lib/helix/runtime
+export EDITOR=hx
 export QT_STYLE_OVERRIDE=kvantum
 export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
