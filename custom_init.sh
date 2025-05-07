0abe7c4c714617061fe0c7289a3b6ad307154bb4
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2025-05-07 13:52:01 +0300

    добаеил загрузку direnv

--- custom_init.sh
+++ custom_init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if command -v fastfetch &> /dev/null; then
-     clear
-     fastfetch
+if command -v fastfetch &>/dev/null; then
+    clear
+    fastfetch
 fi
 
 #
@@ -11,37 +11,41 @@ fi
 
 ### ls
 function ls {
-     if command -v eza &> /dev/null; then
-         eza -1 --icons "$@"
-     else
-         ls "$@"
-     fi
+    if command -v eza &>/dev/null; then
+        eza -1 --icons "$@"
+    else
+        ls "$@"
+    fi
 }
 
 function ll {
-     ls -lh "$@"
+    ls -lh "$@"
 }
 
 function la {
-     ll -a "$@"
+    ll -a "$@"
 }
 
 function tree {
-     if command -v eza &> /dev/null; then
-         ls -T "$@"
-     else
-         tree "$@"
-     fi
+    if command -v eza &>/dev/null; then
+        ls -T "$@"
+    else
+        tree "$@"
+    fi
 }
 
 #
 # Initializations
 #
 
-if command -v starship &> /dev/null; then
-     eval "$(starship init bash)"
+if command -v starship &>/dev/null; then
+    eval "$(starship init bash)"
 fi
 
-if command -v zoxide &> /dev/null; then
-     eval "$(zoxide init --cmd cd bash)"
+if command -v zoxide &>/dev/null; then
+    eval "$(zoxide init --cmd cd bash)"
+fi
+
+if command -v direnv &>/dev/null; then
+    eval "$(direnv hook bash)"
 fi
